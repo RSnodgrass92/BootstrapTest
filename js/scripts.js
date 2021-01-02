bc11486bb15caaae94213f3b964a1a1f0f4e1f43
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-01-02 10:18:29 -0700

    Bootstrap Week 4 Workshop

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,11 @@ $(function()
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+
 });
